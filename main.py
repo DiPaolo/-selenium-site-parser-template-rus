57b54107fe1b69f90fbce466d755bb7046ce81e7
Pavel Ditenbir <pavel.ditenbir@gmail.com>
2022-12-10 14:27:24 +0700

    add some basic logic: print time + save screenshots

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-options = webdriver.ChromeOptions()
-
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# driver = webdriver.Chrome(executable_path="/Users/dipaolo/Downloads/chromedriver", options=options)
 
 driver.get('http://time.is')
-time.sleep(5)
+
+for i in range(0, 5):
+    clock = driver.find_element(By.XPATH, "//time[@id='clock']")
+    print(clock.text)
+    driver.save_screenshot(f'{i}.png')
+    time.sleep(1)
 
 driver.close()
